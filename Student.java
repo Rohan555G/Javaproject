acad254f99f51ef5835fdac8ab125697cdcb8413
Rohan555G <rohan06gupta20@gmail.com>
2025-05-25 22:30:38 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,13 @@
+package model;
+
+public class Student {
+    private String studentId;
+    private String name;
+
+    public Student(String studentId, String name) {
+        this.studentId = studentId;
+        this.name = name;
+    }
+    public String getStudentId() { return studentId; }
+    public String getName() { return name; }
+}
